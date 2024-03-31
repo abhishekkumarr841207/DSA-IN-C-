39274dea12ab0e93aaf8c154a864c096ae6dc3ad
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-03-31 19:02:52 +0530

    first commit

---
+++ BASICS OF C++/operator2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1=6;
+    int num2=3;
+    // airthmetic operators
+    cout<<num1+num2<<endl; 
+    cout<<num1*num2<<endl; 
+    cout<<num1%num2<<endl; 
+    cout<<num1-num2<<endl; 
+    cout<<num1/num2<<endl; 
+
+  // realtional operators
+    cout<<(num1==num2)<<endl; // false
+    cout<<(num1!=num2)<<endl; //true
+    cout<<(num1>=num2)<<endl; // true
+    return 0;
+}
