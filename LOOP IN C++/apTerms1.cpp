d3c7a2738a77401368bcea834e35a3733165cf63
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-03-31 20:53:22 +0530

    add new file

---
+++ LOOP IN C++/apTerms1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value"<<endl;
+   cin>>n;
+    for(int i=1;i<=n;i+=2){
+        cout<<"the arithematic prograssion is :"<<i<<endl;
+    }
+    return 0;
+}
