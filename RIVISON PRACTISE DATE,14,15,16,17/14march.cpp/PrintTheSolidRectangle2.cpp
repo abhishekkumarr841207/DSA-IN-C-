e32ad650f5203daacb8f3c00e553d3bf2734ebf6
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-04-01 19:26:14 +0530

    add new file

---
+++ RIVISON PRACTISE DATE,14,15,16,17/14march.cpp/PrintTheSolidRectangle2.cpp
@@ -0,0 +1,17 @@
+//Print the Solid Square  pattern //
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"enter number";
+    cin>>n;
+
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
