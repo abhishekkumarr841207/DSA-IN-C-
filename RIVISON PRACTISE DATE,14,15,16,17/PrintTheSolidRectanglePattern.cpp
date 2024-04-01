7a5738e55f5b86d61d962b9f5ea7276db61e567e
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-04-01 19:00:02 +0530

    add new file

---
+++ RIVISON PRACTISE DATE,14,15,16,17/PrintTheSolidRectanglePattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int j,i=4;
+    j=3;
+    for(i=1;i<=4;i++)
+    {
+        for(j=1;j<=3;j++){
+            cout<<j;
+        }
+        cout<<endl;
+       
+        
+
+    }
+    return 0;
+}
