7478314f1c92b7bf59c45af747298aefc2fa99ed
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-04-03 16:03:53 +0530

    add new file

---
+++ RIVISION PRACTISE DATE=22,23,24,25/arrayPractise6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,array[6];
+    for(i=0;i<6;i++){
+        cin>>array[i];
+    }
+    for(i=0;i<6;i++){
+        cout<<array[i];
+    }
+    return 0;
+}
